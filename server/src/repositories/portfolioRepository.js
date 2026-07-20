546de43eb071bfef4f06d31feb42bdb2b3a65943
rajneeshyash829@gmail.com <yash.kumar235@lpu.in>
2026-07-20 15:54:38 +0530

    feat: add portfolio repository

---
+++ server/src/repositories/portfolioRepository.js
@@ -0,0 +1,28 @@
+const Portfolio = require("../models/Portfolio");
+
+class PortfolioRepository {
+    async findByUser(userId) {
+        return Portfolio.findOne({ user: userId });
+    }
+
+    async create(userId) {
+        return Portfolio.create({
+            user: userId,
+            holdings: [],
+        });
+    }
+
+    async save(portfolio) {
+        return portfolio.save();
+    }
+
+    async deleteHolding(portfolio, holdingId) {
+        portfolio.holdings = portfolio.holdings.filter(
+            (holding) => holding._id.toString() !== holdingId
+        );
+
+        return portfolio.save();
+    }
+}
+
+module.exports = new PortfolioRepository();
